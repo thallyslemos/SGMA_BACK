cad7044cea7ffa0e0dde4ca6c91f23fd38553cc5
Thallys <thallysvilemos@gmail.com>
2023-04-05 14:23:36 -0300

    .

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  console.log(process.env.DATABASE_URL);
 
   app.useGlobalPipes(
     new ValidationPipe({
